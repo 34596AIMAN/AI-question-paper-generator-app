8afb265e607a36647bde09b9328d7b08ce240ec1
hadiach44556-cyber <hadiach44556@gmail.com>
2026-05-17 20:55:24 +0500

    Create GenerateFragment.java

---
+++ GenerateFragment.java
@@ -0,0 +1,21 @@
+package com.myapp.aiqpg.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.myapp.aiqpg.R;
+
+public class GenerateFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_generate, container, false);
+    }
+}
