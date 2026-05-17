04bb3bc244a730e5d80de6fcd4f08902549aacd1
hadiach44556-cyber <hadiach44556@gmail.com>
2026-05-17 21:06:15 +0500

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,11 @@
+package com.myapp.aiqpg.utils;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://api.example.com/";
+    public static final String DATABASE_NAME = "aiqpg_database";
+    public static final String PREFS_NAME = "aiqpg_prefs";
+
+    private Constants() {
+    }
+}
