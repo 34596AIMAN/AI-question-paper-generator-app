53a0563b566d46d7c19c55de757dbb531484bf27
hadiach44556-cyber <hadiach44556@gmail.com>
2026-05-17 21:06:46 +0500

    Create NetworkUtils.java

---
+++ NetworkUtils.java
@@ -0,0 +1,20 @@
+package com.myapp.aiqpg.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkUtils {
+
+    private NetworkUtils() {
+    }
+
+    public static boolean isInternetAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+            return activeNetwork != null && activeNetwork.isConnected();
+        }
+        return false;
+    }
+}
